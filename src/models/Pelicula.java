0d5c13767c5d611284dca5b9c7cb771c3fdb2ece
tavoxr <tavoxr23@gmail.com>
2024-10-29 21:19:29 -0600

    clase pelicula

--- src/models/Pelicula.java
+++ src/models/Pelicula.java
@@ -4,10 +4,88 @@
  */
 package models;
 
+import java.util.Date;
+
 /**
  *
  * @author Tavox
  */
 public class Pelicula {
+    
+    private int codigo;
+    private String titulo;
+    private String genero;
+    private String director;
+    private Date fecha;
+
+    public Pelicula() {
+    }
+    
+
+    public Pelicula(int codigo, String titulo, String genero, String director, Date fecha) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.director = director;
+        this.fecha = fecha;
+    }
+
+    public Pelicula(String titulo, String genero, String director, Date fecha) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.director = director;
+        this.fecha = fecha;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "codigo=" + codigo + ", titulo=" + titulo + ", genero=" + genero + ", director=" + director + ", fecha=" + fecha + '}';
+    }
+    
+    
+    
+    
+    
+    
     
 }
